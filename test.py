0dcd75ee0f98078ac039733fe4d70c2eab916724
Balaji-ae24m008 <ae24m008@smail.iitm.ac.in>
2026-02-09 08:50:17 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+import pytest
+from code import factorial
+
+
+def test_factorial_zero():
+    assert factorial(0) == 1
+
+
+def test_factorial_one():
+    assert factorial(1) == 1
+
+
+def test_factorial_small():
+    assert factorial(5) == 120
+
+
+def test_factorial_medium():
+    assert factorial(7) == 5040
+
+
+def test_negative_input():
+    with pytest.raises(ValueError):
+        factorial(-5)
