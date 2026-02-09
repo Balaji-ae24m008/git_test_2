d7f8e6e87c990e43b083121dbeee3cbdd1cf548d
Balaji-ae24m008 <ae24m008@smail.iitm.ac.in>
2026-02-09 08:49:29 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+def factorial(n):
+    """
+    Returns factorial of n
+    """
+
+    if n < 0:
+        raise ValueError("Negative input not allowed")
+
+    if n == 0 or n == 1:
+        return 1
+
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+
+    return result
